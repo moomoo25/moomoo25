15e569da9f520c4cb1971a4ea9297c2963e4b5db
moomoo25 <mooja_25@hotmail.com>
2016-04-28 17:26:52 +0700

    #02 สร้าง spaceship

---
+++ GameEngine.java
@@ -0,0 +1,76 @@
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.swing.Timer;
+
+
+public class GameEngine implements GameReporter{
+	GamePanel gp;
+		
+	private ArrayList<Enemy> enemies = new ArrayList<Enemy>();	
+	private SpaceShip v;	
+	
+	private Timer timer;
+	
+	private long score = 0;
+	private double difficulty = 0.1;
+	
+	public GameEngine(GamePanel gp, SpaceShip v) {
+		this.gp = gp;
+		this.v = v;		
+		
+		gp.sprites.add(v);
+		
+		timer = new Timer(50, new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				process();
+			}
+		});
+		timer.setRepeats(true);
+		
+	}
+	
+	public void start(){
+		timer.start();
+	}
+	
+	
+	
+	private void process(){
+		
+		
+		
+		
+		gp.updateGameUI(this);
+		
+		Rectangle2D.Double vr = v.getRectangle();
+		Rectangle2D.Double er;
+		for(Enemy e : enemies){
+			er = e.getRectangle();
+			if(er.intersects(vr)){
+				die();
+				return;
+			}
+		}
+	}
+	
+	public void die(){
+		timer.stop();
+	}
+	
+	
+
+	public long getScore(){
+		return score;
+	}
+	
+	
+}
--- Main.java
+++ Main.java
@@ -9,6 +9,14 @@ public class Main {
 		frame.setSize(400, 650);
 		frame.getContentPane().setLayout(new BorderLayout());
 		
+		SpaceShip v = new SpaceShip(180, 550, 20, 20);
+		GamePanel gp = new GamePanel();
+		GameEngine engine = new GameEngine(gp, v);
+		
+		frame.getContentPane().add(gp, BorderLayout.CENTER);
 		frame.setVisible(true);
+		
+		engine.start();
+		
 	}
 }
